c5c96d12dfb5144696c9313c5f5a5fad5a882efe
dunabia <161461530+dunabia@users.noreply.github.com>
2025-07-01 14:35:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1151033";
+  let InstanceId_Widget = "405564";
   let InstanceId_QA = "";
-  let InstanceId_SEO = "1154528";
-  let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1154531";
+  let InstanceId_SEO = "";
+  let InstanceId_Carousel = "464049";
+  let InstanceId_StarRating = "";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "REPAIR-ME-RINSE";
+  let ProductId = "7755117920502";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
